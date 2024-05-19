113cb725d5af4d4d3b6cc405917e2f8e51a7e616
shirin44 <S3983427@rmit.edu.vn>
2024-05-19 16:17:49 +0700

    server

--- server/sever.py
+++ server/sever.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 import os
@@ -18,36 +17,10 @@ import random
 app = Flask(__name__)
 CORS(app)
 
-# Load KNN model and ResNet model
-knn_model = load("KNN_ResNet_V2S.joblib")
-resnet_model = load_model("ResNet_Furniture_Classification_local.h5", compile=False)
-resnet_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-# Load ResNet models
-resnet_furniture_model = load_model("ResNet_Furniture_Classification.h5", compile=False)
-resnet_style_model = load_model("ResNet_StyleOnly_Classification.h5", compile=False)
-resnet_duo_model = load_model("ResNet_Duo_Classification.h5", compile=False)
-
-# Preprocess the image
-def preprocess_image3(image):
-    resized_img = tf.image.resize(image, [224, 224])
-    processed_img = resized_img / 255.0
-    expanded_img = tf.expand_dims(processed_img, axis=0)
-    return expanded_img
-
-def classify_image(image):
-    # Predict category and style labels
-    category_label, style_label = resnet_furniture_model.predict(image), resnet_style_model.predict(image)
-
-    # Get the category and style names from the labels
-    category_name = category_labels[np.argmax(category_label)]
-    style_name = style_labels[np.argmax(style_label)]
-
-    return category_name, style_name
-
-
-
-# Define class labels for categories and styles
-category_labels = {
+# Constants
+FURNITURE_DATA_DIR = "Furniture_Data"
+IMAGE_DIR = "Furniture_Data"
+CATEGORY_LABELS = {
     0: "Beds",
     1: "Chairs",
     2: "Dressers",
@@ -56,7 +29,7 @@ category_labels = {
     5: "Tables"
 }
 
-style_labels = {
+STYLE_LABELS = {
     0: "Asian",
     1: "Beach",
     2: "Contemporary",
@@ -64,7 +37,7 @@ style_labels = {
     4: "Eclectic",
     5: "Farmhouse",
     6: "Industrial",
-    7: "Media",
+    7: "Mediterranean",
     8: "Midcentury",
     9: "Modern",
     10: "Rustic",
@@ -76,7 +49,58 @@ style_labels = {
     16: "Victorian"
 }
 
-# Function to get a list of random image paths from the specified directory
+# Load models
+knn_model = load("KNN_ResNet_V2S.joblib")
+resnet_model = load_model("ResNet_Furniture_Classification.h5", compile=False)
+resnet_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+resnet_furniture_model = load_model("ResNet_Furniture_Classification.h5", compile=False)
+
+
+chairs_style_model = load_model("task3/Chairs_Style_Classification.h5", compile=False)
+chairs_style_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+sofas_style_model = load_model("task3/ResNet_Furniture_Classification_sofasv4.h5", compile=False)
+sofas_style_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+beds_style_model = load_model("task3/Beds_Style_Classification.h5", compile=False)
+beds_style_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+tables_style_model = load_model("task3/Tables_Style_Classification.h5", compile=False)
+tables_style_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+
+dressers_style_model = load_model("task3/ResNet_Furniture_Classification_dressersv2.h5", compile=False)
+dressers_style_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Preprocess the image
+def preprocess_image3(image):
+    resized_img = tf.image.resize(image, [224, 224])
+    processed_img = resized_img / 255.0
+    expanded_img = tf.expand_dims(processed_img, axis=0)
+    return expanded_img
+
+def classify_image(image):
+    category_label = resnet_furniture_model.predict(image)
+    category_name = CATEGORY_LABELS[np.argmax(category_label)]
+
+    if category_name == "Chairs":
+        style_label = chairs_style_model.predict(image)
+    elif category_name == "Beds":
+        style_label = beds_style_model.predict(image)
+    elif category_name == "sofas":
+        style_label = sofas_style_model.predict(image)
+    elif category_name == "tables":
+        style_label = tables_style_model.predict(image)
+    elif category_name == "dressers":
+        style_label = dressers_style_model.predict(image)
+    
+
+    style_name = STYLE_LABELS[np.argmax(style_label)]
+
+    return category_name, style_name
+
+# Get random images from directory
 def get_random_images(directory, num_images=10):
     images = os.listdir(directory)
     random_images = random.sample(images, min(num_images, len(images)))
@@ -86,65 +110,42 @@ def get_random_images(directory, num_images=10):
 @app.route('/api/recommendations_task3', methods=['POST'])
 def get_recommendations_task3():
     try:
-        # Get the uploaded image file from the request
         uploaded_image = request.files['image']
-
-        # Open the uploaded image file as a PIL image
         img = Image.open(uploaded_image)
-        img = img.resize((224, 224))  # Resize to match the model input size
+        img = img.resize((224, 224))
         img_array = img_preprocessing.img_to_array(img)
         img_array = np.expand_dims(img_array, axis=0)
-        img_array = img_array / 255.0  # Normalize pixel values
+        img_array = img_array / 255.0
 
-        # Classify the image
         category_name, style_name = classify_image(img_array)
 
-        # Log the category and style labels
-        print("Category Name:", category_name)
-
-
-        print("Style:", style_name)
-
-        # Generate paths for category and style folders
-        category_folder = os.path.join("Furniture_Data", category_name)
+        category_folder = os.path.join(FURNITURE_DATA_DIR, category_name)
         style_folder = os.path.join(category_folder, style_name)
 
-        # Get 10 random images from the style folder
         recommended_images = get_random_images(style_folder, num_images=10)
 
-        # Log the chosen images
-        print("Chosen Images:", recommended_images)
-
-        # Prepare response
         response_data = {
-            'predictedCategoryStyle': f"{category_name}_{style_name}",
+            'predictedCategory': category_name,
+            'predictedStyle': style_name,
             'recommendations': recommended_images
         }
-
-        # Log the response data
-        print("Response Data:", response_data)
 
         return jsonify(response_data)
     except Exception as e:
         print(f"Error: {str(e)}")
-        traceback.print_exc()  # Print the traceback for detailed error information
+        traceback.print_exc()
         return jsonify(error='Failed to process request'), 500
-
-
 
 # Load image DataFrame
 image_df = pd.read_csv("df.csv")
-
-# Define directory for serving images
-IMAGE_DIR = "Furniture_Data"
 
 # Preprocess the uploaded image
 def preprocess_image(img):
     img = img.resize((224, 224))
     img_array = img_preprocessing.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array / 255.0  # Normalize pixel values
-    img_array = preprocess_input(img_array)  # Ensure consistent preprocessing
+    img_array = img_array / 255.0
+    img_array = preprocess_input(img_array)
     return img_array
 
 # Extract features from the preprocessed image
@@ -157,24 +158,15 @@ def find_similar_images(input_features, knn_model, k=10):
     distances, indices = knn_model.kneighbors(input_features.reshape(1, -1), n_neighbors=k)
     return distances, indices
 
-# Define route to handle recommendations request
+# Route to handle recommendations request
 @app.route('/api/recommendations', methods=['POST'])
 def get_recommendations():
     try:
-        # Get the uploaded image file from the request
         uploaded_image = request.files['image']
-
-        # Preprocess the uploaded image
         img = Image.open(uploaded_image)
         img_array = preprocess_image(img)
-
-        # Extract features using ResNet model
         input_image_features = extract_features(img_array)
-
-        # Query k-NN model for recommendations
         distances, indices = find_similar_images(input_image_features, knn_model)
-
-        # Get filenames and similarity scores of recommended images
         recommended_images = []
 
         for index in indices[0]:
@@ -182,25 +174,18 @@ def get_recommendations():
                     
             if not os.path.exists(image_path):
                 print(f"Image not found at path: {image_path}")
-                continue  # Skip this image if the file does not exist
+                continue
 
             similar_img = Image.open(image_path)
             similar_img_resized = similar_img.resize((224, 224))
             similar_image_features = extract_features(np.expand_dims(img_preprocessing.img_to_array(similar_img_resized) / 255.0, axis=0))
             similarity_score = cosine_similarity([input_image_features], [similar_image_features])[0][0]
             recommended_images.append({'path': image_path, 'score': float(similarity_score)})
-            print("Recommended Image Features:", similar_image_features)
 
-        # Sort recommended images by similarity score
         recommended_images.sort(key=lambda x: x['score'], reverse=True)
-
-        # Select the top 10 recommendations
         top_10_recommendations = recommended_images[:10]
-
-        # Extracted features to be sent along with recommendations
         extracted_features = input_image_features.tolist()
 
-        # Prepare response
         response_data = {
             'recommendations': top_10_recommendations,
             'extracted_features': extracted_features
@@ -209,7 +194,7 @@ def get_recommendations():
         return jsonify(response_data)
     except Exception as e:
         print(f"Error: {str(e)}")
-        traceback.print_exc()  # Print the traceback for detailed error information
+        traceback.print_exc()
         return jsonify(error='Failed to process request'), 500
 
 # Route to serve images
@@ -229,7 +214,3 @@ def get_image(filepath):
 if __name__ == '__main__':
     print("Starting the Flask server...")
     app.run(debug=True)
-
-
-
-
